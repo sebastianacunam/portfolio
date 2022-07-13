4f0800a8f66cf198dfea31e228269dc3ad6f1b83
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2022-07-13 20:47:30 -0300

    a ver

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './intro.css'
+import sebas from '../../img/Sebas-removebg.png'
+
+const Intro = () => {
+  return (
+    <div className='i'>
+        <div className='i-left'>
+            <div className='i-left-wrapper'>
+                <h2 className='i-intro'>Hi! My name is</h2>
+                <h1 className='i-name'>Sebastián Acuña</h1>
+                <div className="i-titles">
+                    <div className="i-title-wrapper">
+                        <div className="i-title-item">fullstack web developer</div>
+                        <div className="i-title-item">frontend and backend</div>
+                        <div className="i-title-item">creative</div>
+                        <div className="i-title-item">communicative</div>
+                        <div className="i-title-item">coffee drinker☕</div>
+                    </div>
+                </div>
+                <div className='i-desc'>
+                <h4>
+                    I develop websites for customers of all sizes.
+                    I love teamwork, I really like to interact 
+                    with my co-workers and I am very good at communication! 
+                    I'm also, always, willing to learn new technologies.                 
+                </h4>
+                </div>
+            </div>    
+        </div>
+        {/* this will let me remind the separation between the left and right side */}
+        <div className='i-right'>
+            <div className='i-bg'></div>
+            <img className='i-img' src={sebas} alt="Img not found" />    
+        </div>
+    </div>
+  )
+}
+
+export default Intro
