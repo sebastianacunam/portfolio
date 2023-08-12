b945cec6c96bab174a97cf30e15932024472862f
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2023-08-12 11:18:08 -0300

    update new projects

--- src/data.js
+++ src/data.js
@@ -1,6 +1,9 @@
 import picountries from './img/picountries.png'
 import pifood from './img/pifood.png'
 import esocial from './img/esocial.png'
+import citadina from './img/citadina.png'
+import prix from './img/prix.png'
+import vitabiosa from './img/vitabiosa.png'
 
 export const products = [
     {
@@ -17,5 +20,20 @@ export const products = [
         id: 3,
         img: esocial,
         link: "https://e-social-book.netlify.app/"
+    },
+    {
+        id: 4,
+        img: citadina,
+        link: "https://alojamiento-citadina.vercel.app/"
+    },
+    {
+        id: 5,
+        img: prix,
+        link: "https://prix.com.ar/"
+    },
+    {
+        id: 6,
+        img: vitabiosa,
+        link: "https://vitabiosa.com.ar/"
     }
 ]
